cfa293d809ddaa002e6f8fca7c85d1576d6c0592
unknown <adithyak@BANLADITHYAK01.citrite.net>
2014-11-29 17:21:20 +0530

    Added save cleaned image without using the JAR file

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -9,6 +9,7 @@ class ImagesController < ApplicationController
 
   	if @image.save
   		# yo yo
+  		save_cleaned
   		redirect_to @image
   	else
   		render 'new'
@@ -30,5 +31,16 @@ class ImagesController < ApplicationController
 	def image_params
 	  params.require(:image).permit(:name, :avatar)
 	end
+
+	def save_cleaned
+	  	currentLocation = 'F:\TA\public' + @image.avatar_url.to_s
+	  	currentLocation.gsub! '/','\\'
+		splitUp  = currentLocation.split('.')
+		newImageURL = splitUp[0] + '-cleaned.' + splitUp[1] 
+	  	cmd = 'copy ' + currentLocation + ' ' + newImageURL
+	  	if system(cmd)
+	  		@image.update(cleaned_image: newImageURL)
+	  	end
+	end		
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141129090334) do
+ActiveRecord::Schema.define(version: 20141129114337) do
 
   create_table "images", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "avatar"
+    t.string   "cleaned_image"
   end
 
 end
